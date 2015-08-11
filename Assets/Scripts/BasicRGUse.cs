6e8d1ea7409ca8b49dbe32ab9bb7c3eb4c4f342d
pandaboy <oconnob6@tcd.ie>
2015-08-11 19:52:52 +0100

    Example of using data from graph

---
+++ Assets/Scripts/BasicRGUse.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using RelationshipGraph;
+
+public class BasicRGUse : MonoBehaviour {
+
+    public EntityType entityType;
+    public RelationshipType relationshipType;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
