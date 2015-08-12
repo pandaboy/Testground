d84c6ff314b6edd503f697241dad363d0051ab4e
Brendan O'Connor <sp.brendan@gmail.com>
2015-08-12 12:39:12 +0100

    Basic Scripts for Entity and changing states

---
+++ Assets/Scripts/MonoEntity.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using RelationshipGraph.Interfaces;
+
+public class MonoEntity : MonoBehaviour, INode<MonoEntity>
+{
+    protected static int count = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/StateScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum States
+{
+    NEUTRAL = 0,
+    AGGRESSIVE,
+    SUPPORTIVE
+}
+
+public class StateScript : MonoBehaviour {
+
+    public States state = States.NEUTRAL;
+
+	// Use this for initialization
+	void Start () {
+        ChangeColor();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void ChangeState(States newState)
+    {
+        state = newState;
+        ChangeColor();
+    }
+
+    void ChangeColor()
+    {
+        Color newColor;
+        switch(this.state)
+        {
+            case States.AGGRESSIVE: newColor = new Color(0.8f, 0.1f, 0.1f); break;
+            case States.SUPPORTIVE: newColor = new Color(0.1f, 0.1f, 0.8f); break;
+            case States.NEUTRAL:
+            default:
+                newColor = new Color(0.8f, 0.8f, 0.8f);
+                break;
+        }
+
+        GetComponent<Renderer>().material.color = newColor;
+    }
+}
